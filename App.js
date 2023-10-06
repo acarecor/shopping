c88810d23a98382128a15798424ab03fbc12e70d
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-06 14:23:29 +0200

    created function alert connection status

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator ();
+
 import ShoppingLists from './components/ShoppingLists';
 import Welcome from './components/Welcome';
 
-const Stack = createNativeStackNavigator ();
+import { useNetInfo } from '@react-native-community/netinfo';
+import { useEffect } from 'react';
+import { LogBox, Alert } from 'react-native';
 
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
 
+    //function will display an alert popup if connection is lost 
+    const connectionStatus = useNetInfo();
+  
+    useEffect(() => {
+      if (connectionStatus.isConnected === false)
+     { Alert.alert("Connection lost!");
+     disableNetwork(db);
+     } else if (connectionStatus.isConnected=== true)
+     { enableNetwork(db);
+    }} , [connectionStatus.isConnected]);
 
 const App = ()=> {
   const firebaseConfig = {
@@ -24,6 +38,8 @@ const App = ()=> {
     // Initialize Cloud Firestore and get a reference to the service
     const db = getFirestore(app);
 
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator 
@@ -34,7 +50,8 @@ const App = ()=> {
          />
         <Stack.Screen
         name= "ShoppingLists">
-         {props => <ShoppingLists db={db} {...props} /> }
+         {props => <ShoppingLists isConnected={connectionStatus.isConnected}
+         db={db} {...props} /> }
         </Stack.Screen>
         
       </Stack.Navigator>
